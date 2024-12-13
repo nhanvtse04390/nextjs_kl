efb5cef207c4c948e69eb2b61afaf9aabdd8a927
tungdv <tungdv@mor.com.vn>
2024-12-13 14:38:43 +0700

    first commit

---
+++ my-nextjs-project/src/app/about.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <div className="bg-amber-200">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start bg-amber-200 text-xl">
+        about
+      </main>
+    </div>
+  );
+}
---
+++ my-nextjs-project/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="bg-amber-200">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start bg-amber-200 text-xl">
+        home page
+      </main>
+    </div>
+  );
+}
